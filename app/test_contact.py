75ec47b69b7477994130fbd3a2f175871d441262
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-08-17 23:22:56 +0800

    add pep8 style

--- app/test_contact.py
+++ app/test_contact.py
@@ -1,35 +1,36 @@
 import unittest
 import os
-from app.contact import ContactManager,ConfigFileParseException
+from app.contact import ContactManager, ConfigFileParseException
 import app.config as Config
 
+
 class TestContactManager(unittest.TestCase):
-  def setUp(self):
-    pass
-  def tearDown(self):
-    if os.path.isfile(Config.DEFAULT_DB_FILE):
-      os.remove(Config.DEFAULT_DB_FILE)
+    def setUp(self):
+        pass
 
-  def test_init_db_file_with_filename(self):
-    cm = ContactManager(db_file="test.db")
-    isDBFileExist = os.path.isfile("test.db")
-    self.assertTrue(isDBFileExist)
-    os.remove("test.db")
+    def tearDown(self):
+        if os.path.isfile(Config.DEFAULT_DB_FILE):
+            os.remove(Config.DEFAULT_DB_FILE)
 
-  def test_init_db_file_with_default(self):
-    cm = ContactManager()
-    isDBFileExist = os.path.isfile(Config.DEFAULT_DB_FILE)
-    self.assertTrue(isDBFileExist)
+    def test_init_db_file_with_filename(self):
+        _ = ContactManager(db_file="test.db")
+        isDBFileExist = os.path.isfile("test.db")
+        self.assertTrue(isDBFileExist)
+        os.remove("test.db")
 
-  def test_read_db_file_with_right_format(self):
-    cm = ContactManager(db_file="./test_data/contact.db")
-    self.assertEqual(cm.status, Config.READ_DB_FILE_SUCCESS)
-  
-  def test_read_db_file_with_fail_format(self):
-    with self.assertRaises(ConfigFileParseException):
-      cm = ContactManager(db_file="./test_data/failed_contact.db")
+    def test_init_db_file_with_default(self):
+        _ = ContactManager()
+        isDBFileExist = os.path.isfile(Config.DEFAULT_DB_FILE)
+        self.assertTrue(isDBFileExist)
 
+    def test_read_db_file_with_right_format(self):
+        cm = ContactManager(db_file="./test_data/contact.db")
+        self.assertEqual(cm.status, Config.READ_DB_FILE_SUCCESS)
+
+    def test_read_db_file_with_fail_format(self):
+        with self.assertRaises(ConfigFileParseException):
+            _ = ContactManager(db_file="./test_data/failed_contact.db")
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
